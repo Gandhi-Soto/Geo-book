4f507abc5bd4c6211800ef45bbd5782d27902604
Caleb Martinez Cavazos <caleb.martinez@codigogeek.com.mx>
2024-06-19 16:15:19 -0600

    Creacion del back

---
+++ src/DbConfig.js
@@ -0,0 +1,9 @@
+// <!--################################################################################
+//   --# Nombre del Programa : GeoBooker                                              #
+//   --# Autor               : Caleb Martinez Cavazos                                 #
+//   --# Compania            : Codigo Geek                                            #
+//   --# Proyecto            : GeoBooker                       Fecha: 18/06/2024      #
+//   --# Descripcion General : Variable de conexión para la base de datos en el front #
+//   ---------------------------------------------------------------------------------#-->
+
+export const DB_CONNECTION = "http://localhost:5000";
